3130da64cf744cd0938f429f355977711bca8f2c
thomasquintana <quintana.thomas@gmail.com>
2015-12-18 18:17:35 -0500

    Refactoring server.py

--- applications/examples/heartbeat.py
+++ applications/examples/heartbeat.py
@@ -50,6 +50,7 @@ class Monitor(Switchlet):
 
   def on_receive(self, message):
     message = message.get('body')
+    self.__logger__.info(message.__class__)
     if isinstance(message, InitializeSwitchletEvent):
       self.__update__(message)
     elif isinstance(message, Event):
