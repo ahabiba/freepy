429abff15d60db8d9ce82c95d5e2dd13e1c14481
thomasquintana <quintana.thomas@gmail.com>
2015-12-19 02:14:16 -0500

    Refactoring server.py

---
+++ applications/examples/http.py
@@ -0,0 +1,37 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Thomas Quintana <quintana.thomas@gmail.com>
+
+from lib.esl import Event
+from lib.switchlet import *
+from twisted.web.server import Request
+
+import json
+import logging
+
+class HelloWorld(Switchlet):
+  def __init__(self, *args, **kwargs):
+    super(HelloWorld, self).__init__(*args, **kwargs)
+
+  def on_receive(self, message):
+    message = message.get('body')
+    if isinstance(message, Request):
+      if message.method == 'GET':
+        message.setResponseCode(200)
+        message.write('Hello World!')
+        message.finish()
