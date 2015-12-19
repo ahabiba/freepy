0debc9e88089498ad1ccfe624226b524aba8484b
thomasquintana <quintana.thomas@gmail.com>
2015-12-19 01:35:12 -0500

    Refactoring server.py

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="freepy",
-    version="1.0.1",
-    packages=find_packages('src'),
-    package_dir = {'':'src'},
-    install_requires=['pykka == 1.2.1', 'llist == 0.4', 'twisted == 15.5.0'],
+    name = "freepy",
+    version = "1.0.1",
+    packages = find_packages('src'),
+    package_dir = { '':'src' },
+    install_requires=[ 'jinja2 == 2.8', 'llist == 0.4',
+                       'pykka == 1.2.1', 'twisted == 15.5.0' ],
     include_package_data = True,
-    author="Thomas Quintana",
-    author_email="quintana.thomas@gmail.com",
-    license="Apache License 2.0",
-    url="https://github.com/thomasquintana/freepy",
-    entry_points={
+    author = "Thomas Quintana",
+    author_email = "quintana.thomas@gmail.com",
+    license = "Apache License 2.0",
+    url = "https://github.com/thomasquintana/freepy",
+    entry_points = {
         'console_scripts': [
           'freepy-server = freepy.run:main',
         ]
